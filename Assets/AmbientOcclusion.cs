7e0915f48521b9086b673008cd672d0b6c1daa59
Keijiro Takahashi <keijiro@gmail.com>
2017-06-19 16:42:40 +0900

    WIP: adding setup passes

--- Assets/AmbientOcclusion.cs
+++ Assets/AmbientOcclusion.cs
@@ -4,7 +4,7 @@ using UnityEngine.Rendering;
 namespace ComputeAO
 {
     [RequireComponent(typeof(Camera))]
-    public class AmbientOcclusion : MonoBehaviour
+    public sealed class AmbientOcclusion : MonoBehaviour
     {
         #region Exposed attributes
 
@@ -15,17 +15,29 @@ namespace ComputeAO
 
         #region Built-in resources
 
-        [SerializeField, HideInInspector] Shader _shader;
-        [SerializeField, HideInInspector] ComputeShader _compute;
+        [SerializeField, HideInInspector] ComputeShader _setup1Compute;
+        [SerializeField, HideInInspector] ComputeShader _setup2Compute;
+        [SerializeField, HideInInspector] ComputeShader _aoCompute;
+        [SerializeField, HideInInspector] Shader _debugShader;
 
         #endregion
 
         #region Temporary objects
 
         RenderTexture _aoBuffer;
-        Material _blitMaterial;
+        RenderTexture _linearDepthBuffer;
+        RenderTexture _downsizedDepthBuffer1;
+        RenderTexture _downsizedDepthBuffer2;
+        RenderTexture _downsizedDepthBuffer3;
+        RenderTexture _downsizedDepthBuffer4;
+        RenderTexture _tiledDepthBuffer1;
+        RenderTexture _tiledDepthBuffer2;
+        RenderTexture _tiledDepthBuffer3;
+        RenderTexture _tiledDepthBuffer4;
         CommandBuffer _aoCommand;
-        CommandBuffer _blitCommand;
+
+        Material _debugMaterial;
+        CommandBuffer _debugCommand;
 
         #endregion
 
@@ -34,11 +46,210 @@ namespace ComputeAO
         static void DestroyIfAlive(Object o)
         {
             if (o != null)
+            {
                 if (Application.isPlaying)
                     Destroy(o);
                 else
                     DestroyImmediate(o);
+            }
         }
+
+        #endregion
+
+        #region MonoBehaviour functions
+
+        void Start()
+        {
+            var camera = GetComponent<Camera>();
+
+            // Requires the camera depth texture.
+            camera.depthTextureMode |= DepthTextureMode.Depth;
+
+            // Initialize the AO buffer.
+            _aoBuffer = CreateUAVBuffer(camera.pixelWidth, camera.pixelHeight, 1, 8);
+
+            // Set up the AO command buffer.
+            // It's to be invoked before deferred lighting.
+            _aoCommand = new CommandBuffer();
+            _aoCommand.name = "SSAO";
+            AddDepthSetUpCommands(_aoCommand, camera);
+            AddAOCommands(_aoCommand, camera);
+            camera.AddCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
+
+            // Set up the debug command buffer.
+            // It's to be invoked before image effects.
+            _debugMaterial = new Material(_debugShader);
+            _debugCommand = new CommandBuffer();
+            _debugCommand.name = "SSAO Debug";
+            AddDebugCommands(_debugCommand);
+            camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, _debugCommand);
+        }
+
+        void OnEnable()
+        {
+            if (_aoCommand != null)
+            {
+                var camera = GetComponent<Camera>();
+                camera.AddCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
+                camera.AddCommandBuffer(CameraEvent.BeforeLighting, _debugCommand);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (_aoCommand != null)
+            {
+                var camera = GetComponent<Camera>();
+                camera.RemoveCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
+                camera.RemoveCommandBuffer(CameraEvent.BeforeLighting, _debugCommand);
+            }
+        }
+
+        void OnDestroy()
+        {
+            DestroyIfAlive(_aoBuffer);
+            DestroyIfAlive(_debugMaterial);
+            _aoCommand.Dispose();
+            _debugCommand.Dispose();
+        }
+
+        #endregion
+
+        #region Command buffer builders
+
+        internal static class TextureID
+        {
+            static public readonly int linearDepth = Shader.PropertyToID("LinearZ");
+            static public readonly int downsizedDepth1 = Shader.PropertyToID("DS2x");
+            static public readonly int downsizedDepth2 = Shader.PropertyToID("DS4x");
+            static public readonly int tiledDepth1 = Shader.PropertyToID("DS2xAtlas");
+            static public readonly int tiledDepth2 = Shader.PropertyToID("DS4xAtlas");
+        }
+
+        RenderTexture CreateUAVBuffer(int width, int height, int depth, int bpp)
+        {
+            var format = RenderTextureFormat.R8;
+
+            if (bpp == 16) format = RenderTextureFormat.RHalf;
+            if (bpp == 32) format = RenderTextureFormat.RFloat;
+
+            var rt = new RenderTexture(width, height, 0, format, RenderTextureReadWrite.Linear);
+
+            rt.enableRandomWrite = true;
+
+            if (depth > 1)
+            {
+                rt.dimension = TextureDimension.Tex2DArray;
+                rt.volumeDepth = depth;
+            }
+
+            rt.Create();
+            return rt;
+        }
+
+        // Calculate values in _ZBuferParams (built-in shader variable)
+        // We can't use _ZBufferParams in compute shaders, so this function is
+        // used to give the values in it to compute shaders.
+        Vector4 CalculateZBufferParams(Camera camera)
+        {
+            var fpn = camera.farClipPlane / camera.nearClipPlane;
+            if (SystemInfo.usesReversedZBuffer)
+                return new Vector4(fpn - 1, 1, 0, 0);
+            else
+                return new Vector4(1 - fpn, fpn, 0, 0);
+        }
+
+        void AddDepthSetUpCommands(CommandBuffer cmd, Camera camera)
+        {
+            var w = camera.pixelWidth;
+            var h = camera.pixelHeight;
+
+            // Allocate temporary depth buffers.
+            _linearDepthBuffer = CreateUAVBuffer(w, h, 1, 16);
+            _downsizedDepthBuffer1 = CreateUAVBuffer((w +  1) /  2, (h +  1) /  2, 1, 32);
+            _downsizedDepthBuffer2 = CreateUAVBuffer((w +  3) /  4, (h +  3) /  4, 1, 32);
+            _downsizedDepthBuffer3 = CreateUAVBuffer((w +  7) /  8, (h +  7) /  8, 1, 32);
+            _downsizedDepthBuffer4 = CreateUAVBuffer((w + 15) / 16, (h + 15) / 16, 1, 32);
+            _tiledDepthBuffer1 = CreateUAVBuffer((w +  7) /  8, (h +  7) /  8, 16, 32);
+            _tiledDepthBuffer2 = CreateUAVBuffer((w + 15) / 16, (h + 15) / 16, 16, 32);
+            _tiledDepthBuffer3 = CreateUAVBuffer((w + 31) / 32, (h + 31) / 32, 16, 32);
+            _tiledDepthBuffer4 = CreateUAVBuffer((w + 63) / 64, (h + 63) / 64, 16, 32);
+
+            // 1st downsampling pass.
+            var kernel = _setup1Compute.FindKernel("main");
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "LinearZ", _linearDepthBuffer);
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "DS2x", _downsizedDepthBuffer1);
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "DS4x", _downsizedDepthBuffer2);
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "DS2xAtlas", _tiledDepthBuffer1);
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "DS4xAtlas", _tiledDepthBuffer2);
+            _aoCommand.SetComputeTextureParam(_setup1Compute, kernel, "Depth", BuiltinRenderTextureType.ResolvedDepth);
+            _aoCommand.SetComputeVectorParam(_setup1Compute, "ZBufferParams", CalculateZBufferParams(camera));
+            _aoCommand.DispatchCompute(_setup1Compute, kernel, _tiledDepthBuffer2.width, _tiledDepthBuffer2.height, 1);
+
+            // 2nd downsampling pass.
+            kernel = _setup2Compute.FindKernel("main");
+            _aoCommand.SetComputeTextureParam(_setup2Compute, kernel, "DS4x", _downsizedDepthBuffer2);
+            _aoCommand.SetComputeTextureParam(_setup2Compute, kernel, "DS8x", _downsizedDepthBuffer3);
+            _aoCommand.SetComputeTextureParam(_setup2Compute, kernel, "DS16x", _downsizedDepthBuffer4);
+            _aoCommand.SetComputeTextureParam(_setup2Compute, kernel, "DS8xAtlas", _tiledDepthBuffer3);
+            _aoCommand.SetComputeTextureParam(_setup2Compute, kernel, "DS16xAtlas", _tiledDepthBuffer4);
+            _aoCommand.DispatchCompute(_setup2Compute, kernel, _tiledDepthBuffer4.width, _tiledDepthBuffer4.height, 1);
+        }
+
+        void AddAOCommands(CommandBuffer cmd, Camera camera)
+        {
+        }
+
+        void AddDebugCommands(CommandBuffer cmd)
+        {
+            //_debugCommand.SetGlobalTexture("_AOTexture", _aoBuffer);
+            //_debugCommand.Blit(null, BuiltinRenderTextureType.CurrentActive, _debugMaterial, 0);
+            //_debugCommand.SetGlobalTexture("_AOTexture", _downsizedDepthBuffer4);
+            //_debugCommand.Blit(null, BuiltinRenderTextureType.CurrentActive, _debugMaterial, 0);
+            _debugCommand.SetGlobalTexture("_TileTexture", _tiledDepthBuffer4);
+            _debugCommand.Blit(null, BuiltinRenderTextureType.CurrentActive, _debugMaterial, 1);
+        }
+
+        #endregion
+
+#if lsjdldfjksla
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Execute it before image effects.
+
+        #region Private functions
+            uint sizeX, sizeY, sizeZ;
+            _compute.GetKernelThreadGroupSizes(kernel, out sizeX, out sizeY, out sizeZ);
+
+            _aoCommand.DispatchCompute(
+                _compute, kernel,
+                camera.pixelWidth / (int)sizeX,
+                camera.pixelHeight / (int)sizeY,
+                1
+            );
+            var kernel = _compute.FindKernel("main");
+
+            _aoCommand.SetComputeTextureParam(
+                _compute, kernel, "DepthTex", BuiltinRenderTextureType.ResolvedDepth
+            );
+
+            _aoCommand.SetComputeTextureParam(
+                _compute, kernel, "Occlusion", _aoBuffer
+            );
+
+            UpdateComputeParameters(camera.pixelWidth, camera.pixelHeight, 1 / camera.projectionMatrix[0, 0]);
 
         void UpdateComputeParameters(float width, float height, float TanHalfFovH)
         {
@@ -139,107 +350,10 @@ namespace ComputeAO
             _aoCommand.SetComputeFloatParam(_compute, "gRejectFadeoff", 1 / -_rejectionFalloff);
             _aoCommand.SetComputeFloatParam(_compute, "gRcpAccentuation", 1 / (1 + _accentuation));
 
-            var near = GetComponent<Camera>().nearClipPlane;
-            var far = GetComponent<Camera>().farClipPlane;
-            var vx = 1 - far / near;
-            var vy = far / near;
-            vy += vx;
-            vx = -vx;
-            _aoCommand.SetComputeVectorParam(_compute, "gZBufferParams", new Vector4(
-            vx, vy, 0, 0
-            ));
         }
+
 
         #endregion
-
-        #region MonoBehaviour functions
-
-        void Start()
-        {
-            var camera = GetComponent<Camera>();
-
-            camera.depthTextureMode |= DepthTextureMode.Depth;
-
-            // Allocate a render texture with UAV for storing AO.
-            _aoBuffer = new RenderTexture(
-                camera.pixelWidth, camera.pixelHeight, 0,
-                RenderTextureFormat.R8, RenderTextureReadWrite.Linear
-            );
-            _aoBuffer.enableRandomWrite = true;
-            _aoBuffer.Create();
-
-            // Set up a command buffer for the AO estimator.
-            _aoCommand = new CommandBuffer();
-            _aoCommand.name = "AO Estimator";
-
-            var kernel = _compute.FindKernel("Default");
-
-            _aoCommand.SetComputeTextureParam(
-                _compute, kernel, "DepthTex", BuiltinRenderTextureType.ResolvedDepth
-            );
-
-            _aoCommand.SetComputeTextureParam(
-                _compute, kernel, "Occlusion", _aoBuffer
-            );
-
-            UpdateComputeParameters(camera.pixelWidth, camera.pixelHeight, 1 / camera.projectionMatrix[0, 0]);
-
-            uint sizeX, sizeY, sizeZ;
-            _compute.GetKernelThreadGroupSizes(kernel, out sizeX, out sizeY, out sizeZ);
-
-            _aoCommand.DispatchCompute(
-                _compute, kernel,
-                camera.pixelWidth / (int)sizeX,
-                camera.pixelHeight / (int)sizeY,
-                1
-            );
-
-            // Execute it before lighting.
-            camera.AddCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
-
-            // Set up a command buffer for final composition.
-            _blitCommand = new CommandBuffer();
-            _blitCommand.name = "AO Composition";
-
-            _blitCommand.SetGlobalTexture("_AOTexture", _aoBuffer);
-
-            _blitMaterial = new Material(_shader);
-            _blitCommand.Blit(
-                null, BuiltinRenderTextureType.CurrentActive, _blitMaterial, 0
-            );
-
-            // Execute it before image effects.
-            camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, _blitCommand);
-        }
-
-        void OnEnable()
-        {
-            if (_aoCommand != null)
-            {
-                var camera = GetComponent<Camera>();
-                camera.AddCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
-                camera.AddCommandBuffer(CameraEvent.BeforeLighting, _blitCommand);
-            }
-        }
-
-        void OnDisable()
-        {
-            if (_aoCommand != null)
-            {
-                var camera = GetComponent<Camera>();
-                camera.RemoveCommandBuffer(CameraEvent.BeforeLighting, _aoCommand);
-                camera.RemoveCommandBuffer(CameraEvent.BeforeLighting, _blitCommand);
-            }
-        }
-
-        void OnDestroy()
-        {
-            DestroyIfAlive(_blitMaterial);
-            DestroyIfAlive(_aoBuffer);
-            _aoCommand.Dispose();
-            _blitCommand.Dispose();
-        }
-
-        #endregion
+#endif
     }
 }
---
+++ Assets/Debug.shader
@@ -0,0 +1,93 @@
+﻿Shader "Hidden/ComputeAO/Debug"
+{
+    Properties
+    {
+        _AOTexture("", 2D) = "" {}
+        _TileTexture("", 2DArray) = "" {}
+    }
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex Vert
+            #pragma fragment Frag
+
+            #include "UnityCG.cginc"
+
+            sampler2D _AOTexture;
+
+            struct Attributes
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct Varyings
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            Varyings Vert(Attributes input)
+            {
+                Varyings o;
+                o.vertex = UnityObjectToClipPos(input.vertex);
+                o.uv = input.uv;
+                return o;
+            }
+
+            fixed4 Frag(Attributes input) : SV_Target
+            {
+                return tex2D(_AOTexture, input.uv).r;
+            }
+
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex Vert
+            #pragma fragment Frag
+
+            #include "UnityCG.cginc"
+
+            UNITY_DECLARE_TEX2DARRAY(_TileTexture);
+
+            float _Depth;
+
+            struct Attributes
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct Varyings
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            Varyings Vert(Attributes input)
+            {
+                Varyings o;
+                o.vertex = UnityObjectToClipPos(input.vertex);
+                o.uv = input.uv;
+                return o;
+            }
+
+            fixed4 Frag(Attributes input) : SV_Target
+            {
+                float3 uvw = float3(input.uv, fmod(_Time.y * 4, 16));
+                return UNITY_SAMPLE_TEX2DARRAY(_TileTexture, uvw).r;
+            }
+
+            ENDCG
+        }
+    }
+}
